e156a2128b29c7c63c3b3d0fecf5846c96fce2a6
Jessyjoss <jessica.martinez5764@alumnos.udg.mx>
2025-03-16 16:19:09 -0600

    Mi primer Commit

---
+++ Introduccion js/index.js
@@ -0,0 +1,15 @@
+console.log(typeof 42);         // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666);       // number
+console.log(typeof true);       // boolean
+console.log(typeof 0);          // number
+console.log(typeof '');         // string
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
+console.log(typeof false);      // boolean
+
+// Agregar tus propios ejemplos
+console.log(typeof { name: 'Juan' }); // object
+console.log(typeof [1, 2, 3]);       // object (aunque en realidad es un array)
+console.log(typeof NaN);             // number (es un valor especial de tipo number)
+console.log(typeof function() {});   // function
