aaed1caa820221ed007cbbb1eacf74f420463f6c
Jessyjoss <jessica.martinez5764@alumnos.udg.mx>
2025-04-06 19:01:29 -0600

    Agrego cambios en DOM

---
+++ DOM/Index.js
@@ -0,0 +1,28 @@
+const render = document.getElementById('render');
+const emojis = ['✨', '🎀', '👉', '🎁', '🎆', '😎'];
+const button = document.getElementById('action');
+
+function crearElemento(){
+    let elemento = document.createElement('span')
+    elemento.classList.add('carta')
+    return elemento;
+}
+
+function pintarElemento(){
+    for(let i = 0; i < emojis.length; i++){
+        let elemento = crearElemento();
+        elemento.innerHTML = `${emojis[i]}`
+        render.appendChild(elemento)
+    }
+}
+
+button.addEventListener('click', () => {
+    pintarElemento();
+    render.classList.remove('hide')
+    button.classList.add('hide')
+    setTimeout(()=>{
+        button.classList.remove('hide')
+        render.classList.add('hide')
+        render.innerHTML = '';
+    }, 5000)
+})
